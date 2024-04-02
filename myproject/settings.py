fea5f802a9bb13dab71815c578f6df5b7f79a402
Neo_codez <holyjazzy1@gmail.com>
2024-04-02 09:16:47 +0100

    Squashed some bugs

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uig^xd^lulg4u#k*fm62x*=6bqk2h^c4ox!c8+0%+m0h#0nmg#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rockwell-trust-bank.onrender.com', 'rockwelltrustinvestments.com']
 
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'infothehubterminus@gmail.com'
-EMAIL_HOST_PASSWORD = 'tfhe oshg odci coac'
+EMAIL_HOST = 'mail.privateemail.com'
+EMAIL_HOST_USER = 'support@rockwelltrustinvestments.com'
+EMAIL_HOST_PASSWORD = 'Newstatus@12'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
