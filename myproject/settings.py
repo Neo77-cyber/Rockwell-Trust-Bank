c2d1dfd695680774b03187eaf65318934363d0df
Neo_codez <holyjazzy1@gmail.com>
2024-04-25 12:13:13 +0100

    Updated Allowed Host

--- myproject/settings.py
+++ myproject/settings.py
@@ -35,7 +35,7 @@ DEBUG = os.environ.get('DEBUG', 'True')=="True"
 
 # DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rockwell-trust-bank.onrender.com', 'rockwelltrustinvestments.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'rockwell-trust-bank.onrender.com', 'rockwelltrustinvestments.com', 'rockwell-investment.onrender.com']
 
 
 # Application definition
